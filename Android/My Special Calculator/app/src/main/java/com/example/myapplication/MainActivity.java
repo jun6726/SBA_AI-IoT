6a74d081f4ed7bdd08796a6e7eaead567812c747
YEON <junmarko@naver.com>
2019-08-06 14:13:21 +0900

    My Special Calculator postfix 계산

--- Android/My Special Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/My Special Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Stack;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16;
-    EditText editText;
+public class MainActivity extends AppCompatActivity implements SensorEventListener  {
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, buttonClear, buttonDel;
+    static EditText editText;
     boolean cal;
-    int cal_result;
+    double cal_result;
+
+    private SensorManager sensorManager;
+    private android.hardware.Sensor senAccelerometer;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 700;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +56,19 @@ public class MainActivity extends AppCompatActivity {
         button14 = findViewById(R.id.button14);
         button15 = findViewById(R.id.button15);
         button16 = findViewById(R.id.button16);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonDel = findViewById(R.id.buttonDel);
         editText = findViewById(R.id.editText);
+
+        sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        senAccelerometer = sensorManager.getDefaultSensor(android.hardware.Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editText.setText(editText.getText().toString() + 0);
+
             }
         });
         button1.setOnClickListener(new View.OnClickListener() {
@@ -103,31 +128,46 @@ public class MainActivity extends AppCompatActivity {
         button10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + "=");
+                String result4cal = editText.getText().toString();
+                Calculator calculator = new Calculator();
+                try {
+                    String resultVal = calculator.bracketCalMain(result4cal);
+                    cal = true;
+                    editText.setText(resultVal);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
         button11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + "-");
+                editText.setText(editText.getText().toString() + "/");
             }
         });
         button12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + "+");
+                editText.setText(editText.getText().toString() + "*");
             }
         });
         button13.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + "*");
+                editText.setText(editText.getText().toString() + "+");
             }
         });
         button14.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText().toString() + "/");
+                editText.setText(editText.getText().toString() + "-");
+            }
+        });
+        buttonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result4del = String.valueOf(editText.getText());
+                editText.setText(result4del.substring(0,result4del.length()-1));
             }
         });
         button15.setOnClickListener(new View.OnClickListener() {
@@ -137,8 +177,10 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "계산을 완료하고 눌러주세요.", Toast.LENGTH_SHORT).show();
                 } else if (cal == true) {
                     //팁 계산
-                    cal_result = Integer.parseInt(editText.getText().toString());
-                    cal_result = (int) (cal_result * 1.1);
+                    cal_result =  Double.parseDouble(editText.getText().toString());
+                    cal_result = (double) (cal_result * 1.1);
+                    editText.setText(cal_result+"");
+                    cal = false;
                 }
             }
         });
@@ -150,68 +192,59 @@ public class MainActivity extends AppCompatActivity {
                 } else if (cal == true) {
                     //팁 계산
                     cal_result = Integer.parseInt(editText.getText().toString());
-                    cal_result = (int) (cal_result * 1.15);
+                    cal_result = (cal_result * 1.15);
+                    editText.setText(cal_result+"");
+                    cal = false;
                 }
             }
         });
-    }
-
-    private static int getPriority(String parm1) {
-        switch (parm1) {
-            case "(":
-            case ")":
-                return 0;
-            case "+":
-            case "-":
-                return 1;
-            case "*":
-            case "/":
-                return 2;
-        }
-        return -1;
-
-    }
-
-
-    private static ArrayList<String> toPostfix(String str) {
-
-        ArrayList<String> result = new ArrayList<>();
-        String[] calcTarget = str.split(" ");
-        Stack<String> stack = new Stack<>();
-        String forPrint = "";
-
-        for (int i = 0; i < calcTarget.length; i++) {
-            switch (calcTarget[i]) {
-                case "(":
-                    stack.push(calcTarget[i]);
-                    break;
-
-                case ")":
-                    forPrint = stack.pop();
-                    while (!forPrint.equals("(")) {
-                        result.add(forPrint);
-                        forPrint = stack.pop();
-                    }
-                    break;
-                case "+":
-                case "-":
-                case "/":
-                case "*":
-                    while (!stack.isEmpty() && getPriority(calcTarget[i]) <= getPriority(stack.peek())){
-                        forPrint = stack.pop();
-                        result.add(forPrint);
-                    }
-                    stack.push(calcTarget[i]);
-                    break;
-                    default:
-                        result.add(calcTarget[i]);
-                        break;
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
             }
-            while(!stack.isEmpty()){
-                forPrint = stack.pop();
-                result.add(forPrint);
+        });
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        android.hardware.Sensor mySensor = sensorEvent.sensor;
+        if(mySensor.getType() == android.hardware.Sensor.TYPE_ACCELEROMETER){
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis(); // 현재시간
+            if((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+                if (speed > SHAKE_THRESHOLD) {
+                    //지정된 수치이상 흔들림이 있으면 실행
+                    editText.setText("");
+                } else if (speed < 10) {
+                }
+                //갱신
+                last_x = x;
+                last_y = y;
+                last_z = z;
             }
         }
-        return result;
+    }
+
+    @Override
+    public void onAccuracyChanged(android.hardware.Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
